efff9a254a4b7cc3d79c08beeec8f5a56f7c9462
Beny Hasanudin <bhasanudin@gmail.com>
2019-12-16 13:08:27 +0700

    Inheritence in dart

---
+++ src/orbiter.dart
@@ -0,0 +1,6 @@
+import 'spacecraft.dart';
+
+class Orbiter extends Spacecraft{
+  num altitude;
+  Orbiter(String name, DateTime launchDate, this.altitude):super(name, launchDate);
+}
--- src/variables.dart
+++ src/variables.dart
@@ -1,8 +1,11 @@
 import 'spacecraft.dart';
+import 'orbiter.dart';
 
 void main(){
   var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
   voyager.describe();
   var voyager3 = Spacecraft.unlaunched('Voyager III');
   voyager3.describe();
+  var orbiter = Orbiter('Jupiter I',DateTime(1987,8,27),2000);
+  orbiter.describe();
 }

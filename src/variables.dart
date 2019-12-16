f2a34db432ea40499dda6f2c248b457f05acb0ee
Beny Hasanudin <bhasanudin@gmail.com>
2019-12-16 08:01:03 +0700

    --amend

--- src/variables.dart
+++ src/variables.dart
@@ -2,6 +2,9 @@ void main(){
  var name = "Voyager I";
  var year = 1977;
  var antennaDiameter = 3.7;
- var flybyObjects = ['Jupiter',
-];
+ var flybyObjects = ['Jupiter','Saturn', 'Uranus'];
+  var image = {
+     'tags':['saturn'],
+     'url':'//path/to/saturn.jpg'
+  };
 }
